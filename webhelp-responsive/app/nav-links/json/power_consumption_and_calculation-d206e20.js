2fc913e7edccd3f0ce8cc8d294a7dc6fc16962d5
joyceclc23 <140184861+joyceclc23@users.noreply.github.com>
2023-07-22 15:39:40 +0800

    Add files via upload

---
+++ webhelp-responsive/app/nav-links/json/power_consumption_and_calculation-d206e20.js
@@ -0,0 +1,1 @@
+oxy_webhelp_navlinks = {"power_consumption_and_calculation-d206e20":{"topics":[{"title":"2.1. Power Calculator","href":"power_calculator.html","attributes":{"data-id":"power_calculator"},"menu":{"hasChildren":true},"tocID":"power_calculator-d206e28","next":"power_calculator-d206e28"}]}};
---
+++ webhelp-responsive/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {10:5,7:8,9:8,5:6,3:5,8:-1,6:-1,2:-1,0:-1,4:5};
