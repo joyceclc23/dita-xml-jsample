75b0935e71c75094ed28f539e33a71aa95d63e3f
joyceclc23 <140184861+joyceclc23@users.noreply.github.com>
2023-07-22 15:27:50 +0800

    Add files via upload

---
+++ webhelp-responsive/search/index/keywords.js
@@ -0,0 +1,17 @@
+var keywords=[{w:"4.",p:["p0"]},{w:"Conclusion",p:["p0"]},{w:"Context",p:["p1"]},{w:"Sensitive",p:["p1"]},{w:"Help",p:["p1"]},{w:"1.",p:["p2"]},{w:"Introduction",p:["p2"]},{w:"2.1.2.",p:["p3"]},{w:"Junction",p:["p3"]},{w:"Temperature",p:["p3","p4"]},{w:"2.1.3.",p:["p4"]},{w:"Maximum",p:["p4"]},{w:"Safe",p:["p4"]},{w:"Ambient",p:["p4"]},{w:"2.1.",p:["p5"]},{w:"Power",p:["p5","p6","p9"]},{w:"Calculator",p:["p5","p9"]},{w:"2.",p:["p6"]},{w:"Consumption",p:["p6"]},{w:"and",p:["p6","p7","p10"]},{w:"Calculation",p:["p6"]},{w:"3.1.",p:["p7"]},{w:"Thermal",p:["p7","p8","p9"]},{w:"Impedance",p:["p7"]},{w:"Airflow",p:["p7"]},{w:"3.",p:["p8"]},{w:"Management",p:["p8","p9"]},{w:"3.2.",p:["p9"]},{w:"in",p:["p9"]},{w:"2.1.1.",p:["p10"]},{w:"Typical",p:["p10"]},{w:"Worst",p:["p10"]},{w:"Case",p:["p10"]},{w:"Process",p:["p10"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 3, 4];
+ph["p2"]=[5, 6];
+ph["p3"]=[7, 8, 9];
+ph["p4"]=[10, 11, 12, 13, 9];
+ph["p5"]=[14, 15, 16];
+ph["p6"]=[17, 15, 18, 19, 20];
+ph["p7"]=[21, 22, 23, 19, 24];
+ph["p8"]=[25, 22, 26];
+ph["p9"]=[27, 22, 26, 28, 15, 16];
+ph["p10"]=[29, 30, 19, 31, 32, 33];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
