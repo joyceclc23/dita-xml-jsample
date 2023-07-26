a6a18ef595e47c308d2e7784a3a4687bbb1ccca7
joyceclc23 <140184861+joyceclc23@users.noreply.github.com>
2023-07-26 10:15:11 +0800

    Add files via upload

---
+++ ugsamples/out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,23 @@
+var keywords=[{w:"Adding",p:["p0"]},{w:"Implementations",p:["p0","p4"]},{w:"Basic",p:["p1","p2"]},{w:"Design",p:["p1","p12","p15"]},{w:"Prerequisites",p:["p1"]},{w:"UI",p:["p2"]},{w:"Controls",p:["p2"]},{w:"Clear",p:["p3"]},{w:"Tool",p:["p3"]},{w:"Memory",p:["p3","p14"]},{w:"Cloning",p:["p4"]},{w:"Creating",p:["p5"]},{w:"a",p:["p5"]},{w:"New",p:["p5"]},{w:"Project",p:["p5","p10"]},{w:"Context",p:["p6"]},{w:"Sensitive",p:["p6"]},{w:"Help",p:["p6"]},{w:"Running",p:["p7"]},{w:"the",p:["p7"]},{w:"Radiant",p:["p7","p8","p11","p12","p13"]},{w:"Software",p:["p7","p8","p11","p12","p13","p15"]},{w:"Introduction",p:["p8"]},{w:"to",p:["p8"]},{w:"Lattice",p:["p8"]},{w:"Menus",p:["p9"]},{w:"and",p:["p9","p10"]},{w:"Toolbars",p:["p9"]},{w:"Overview",p:["p10","p15"]},{w:"of",p:["p10"]},{w:"Implementation",p:["p10"]},{w:"Advanced",p:["p11"]},{w:"Features",p:["p11"]},{w:"Environment",p:["p12","p14"]},{w:"User",p:["p13"]},{w:"Interface",p:["p13"]},{w:"Operation",p:["p13"]},{w:"Shared",p:["p14"]},{w:"Flow",p:["p15"]},{w:"Start",p:["p16"]},{w:"Page",p:["p16"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 3, 4];
+ph["p2"]=[2, 5, 6];
+ph["p3"]=[7, 8, 9];
+ph["p4"]=[10, 1];
+ph["p5"]=[11, 12, 13, 14];
+ph["p6"]=[15, 16, 17];
+ph["p7"]=[18, 19, 20, 21];
+ph["p8"]=[22, 23, 24, 20, 21];
+ph["p9"]=[25, 26, 27];
+ph["p10"]=[28, 29, 14, 26, 30];
+ph["p12"]=[20, 21, 3, 33];
+ph["p11"]=[20, 21, 31, 32];
+ph["p14"]=[37, 9, 33];
+ph["p13"]=[20, 21, 34, 35, 36];
+ph["p16"]=[39, 40];
+ph["p15"]=[21, 3, 38, 28];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ ugsamples/out/webhelp-responsive/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {16:13,2:13,4:10,14:11,9:13,0:10,3:14};
